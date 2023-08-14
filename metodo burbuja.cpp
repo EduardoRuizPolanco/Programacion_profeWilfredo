10122f5858a9dcab0d6e44f5227e08e76059f636
EduardoRuizPolanco <136515201+EduardoRuizPolanco@users.noreply.github.com>
2023-08-13 18:26:13 -0600

    Metodo burbuja de las edades.cpp

--- metodo burbuja.cpp
+++ metodo burbuja.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
 #include<conio.h>
 
- using namespace std;
+using namespace std;
 
 int main(){
-	int numeros[] = {1,3,4,5,2};
-	int i,j,aux;
+int edades[10];
+int i,j,aux;
+
+cout<<"Pon las edades de las 10 personas: "<<endl;
+for(int i=0; i<10; i++){
+cin>>edades[i];
+}	
+for(i=0; i<10; i++){
+for(j=0; j<10; j++){
+
+if(edades[j]>edades[j+1]){
+aux = edades[j];
+edades[j]=edades[j+1];
+edades[j+1]	= aux;
+
+}
+}
+}	
 	
-	for(i=0;i<5;i++){
-		for(j=0;j<5;i++){
-			if(numeros[j] > numeros[j+1]){
-				numeros[j] = numeros[j+1];
-				numeros [j+1] = aux;
-			}
-		}
-	}
-	 cout<<"Orden ascendente: ";
-	 for(i=0;i<5;i++){
-	 	cout<<numeros[i]<<" ";
-	
-	 }
-	
-	getch();
-	return 0;
+cout<<"Orden ascendente de las edades: ";
+for(i=0;i<10;i++){
+cout<<edades[i]<<" ";
+
+}
+getch();
+return 0;
 }
